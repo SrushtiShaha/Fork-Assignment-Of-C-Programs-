b7ea63056582d133efb17484f8709246489d8138
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-09-04 18:32:58 +0530

    Add files via upload

---
+++ Basic Programs/Q1 Write A Program For Output As Follow.c
@@ -0,0 +1,22 @@
+/// printf() - Formatted Output Implementation
+
+/// R.N.| Name   | Eng | Hin | Tot | Per
+/// 255 | Amit   |  09 |  81 | 150 | 50.00
+///  02 | Sujata |  70 |  80 | 200 | 66.67
+/// 150 | Ajay   |  82 |  18 | 170 | 56.67
+
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("\n R.N | Name   | Eng | Hin | Tot | Per");
+  printf("\n 255 | Amit   |  09 |  81 | 150 | 50.00");
+  printf("\n  02 | Sujata |  70 |  80 | 200 | 66.67");
+  printf("\n 150 | Ajay   |  82 |  18 | 170 | 56.67\n");
+  
+  getch();
+  return 0;
+}
