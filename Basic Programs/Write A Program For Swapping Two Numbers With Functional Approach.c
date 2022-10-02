694bcdf83a73c15369d915c182367e7f5d59cd7c
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-10-02 20:55:09 +0530

    Add files via upload

---
+++ Basic Programs/Write A Program For Swapping Two Numbers With Functional Approach.c
@@ -0,0 +1,34 @@
+/// Write A Program For Swapping Two Numbers Without Functional Approach.
+ 
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+void Swap( int, int );
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0;
+    
+    printf("\n Enter Two Numbers => ");
+    scanf("%d%d",&iNo1,&iNo2);
+  
+    Swap( iNo1, iNo2 );
+  
+    getch();
+    return 0;
+}
+
+void Swap( int iN1, int iN2 ) // 3 8
+{
+    int iSwap = 0;
+    
+    iSwap = iN1;
+    iN1 = iN2;
+    iN2 = iSwap;
+    
+    printf("\n After Swapping The Values Of Numbe1 = %d And Number2 = %d \n", iN1, iN2);
+ 
+    return;
+}
