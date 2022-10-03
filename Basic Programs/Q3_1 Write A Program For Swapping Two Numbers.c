ff68dd9a9496d2d5011aae58049940f245098ec6
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-10-03 19:47:50 +0530

    Rename Q3.1 Write A Program For Swapping Two Numbers.c to Q3_1 Write A Program For Swapping Two Numbers.c

---
+++ Basic Programs/Q3_1 Write A Program For Swapping Two Numbers.c
@@ -0,0 +1,23 @@
+/// Write A Program For Swapping Two Numbers ( Without Functional Approach ).
+ 
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0, iSwap = 0;
+    
+    printf("\n Enter Two Numbers => ");
+    scanf("%d%d",&iNo1,&iNo2);
+  
+    iSwap = iNo1;
+    iNo1 = iNo2;
+    iNo2 = iSwap;
+  
+    printf("\n After Swapping The Values Of Numbe1 = %d And Number2 = %d \n", iNo1, iNo2);
+  
+    getch();
+    return 0;
+}
