cdf2da01a670aedbe6e84b333f65701680b0a475
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-10-07 23:34:56 +0530

    Add files via upload

---
+++ Conditional Statements And Loops/Q1_1 Write Program To Find Maximum From Two Numbers.c
@@ -0,0 +1,30 @@
+/// Write Program To Find Maximum From 2 Numbers.
+
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0;
+    
+    printf("\n Enter Two Numbers => ");
+    scanf("%d%d",&iNo1,&iNo2);
+    
+    if( iNo1 > iNo2 )
+    {
+        printf("\n %d Is Maximum.\n",iNo1);
+    }
+    else if( iNo2 > iNo1 )
+    {
+        printf("\n %d Is Maximum.\n",iNo2);
+    }
+    else
+    {
+        printf("\n %d And %d Are Equal.\n",iNo1,iNo2);
+    }
+    
+    getch();
+    return 0;
+}
