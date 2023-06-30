9d49d1b3437fd5c98db24852f5284bc07cf66d21
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-07-01 02:20:22 +0800

    Program_Of_Conditional_Control_And_Looping

---
+++ Conditional Statements And Loops/02_B_Write_A_Program_To_Accept_Number_From_User_And_Check_Whether_It_Is_Prime_Or_Not_With_Ternary_Operator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int iNo = 0, i = 0;
+
+        printf("\n Enter A Number To Check It Is Prime Or Not -> ");
+        scanf("%d",&iNo);
+
+        for( i = 2; i < iNo; i++ )
+        {
+                if( iNo % i == 0 )
+                {
+                        break;
+                }
+        }
+
+        ( i == iNo ) ? printf("\n The Entered Number %d Is A Prime Number. \n",iNo) : printf("\n The Entered Number %d Is Not A Prime Number. \n",iNo);
+
+        getch();
+        return 0;
+}
